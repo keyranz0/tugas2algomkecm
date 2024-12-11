d0039bef318aa7f434848d5735d8fcc01cdd45b8
keyranz0 <yogaarthawan13@gmail.com>
2024-12-11 15:07:31 +0800

    Add files via upload

---
+++ tugas2keyran.py
@@ -0,0 +1,26 @@
+#JUDUL
+print("TUGAS 2. KONVERSI SATUAN METER(m) KE CENTIMETER(cm)")
+print("Algoritma dan Logika Informatika")
+
+print() #agar ada spasi antara judul dengan data diri
+
+#DATA DIRI
+#cetaknama
+print("Nama : I Wayan Yoga Arthawan")
+#cetaknim
+print("Nim : 2401010357")
+#cetakprodi
+print("Prodi : IF - KAB")
+ 
+print() #spasi
+
+#JUDUL TUGAS
+print("Masukan jumlah yang mau dikonversi")
+
+print() #spasi
+#meminta user memasukan nilai dalam meter
+meter = float(input("Masukan nilai dalam meter: "))
+#konversi meter ke centimeter(1m=100centimeter)
+centimeter = meter*100
+#menampilkan hasil konversi
+print(f"{meter} meter sama dengan {centimeter} centimeter.")
